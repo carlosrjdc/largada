cd351b390f2bae93b8465271206ccfd286bef143
ROSA Carlos <carlos.rosa@br.lactalis.com>
2023-03-12 15:18:49 -0300

    first commit

--- src/converter/index.js
+++ src/converter/index.js
@@ -1,94 +1,95 @@
-const excelToJson = require('convert-excel-to-json');
-const path = require('path');
-const fs = require('fs');
+const excelToJson = require("convert-excel-to-json");
+const path = require("path");
+const fs = require("fs");
 const db = require("../../models");
-const infoDados = require("./dados.json")
-const colunas = require("./colunas.js")
-const functionAjuste = require("./functionAjuste.js")
+const infoDados = require("./dados.json");
+const colunas = require("./colunas.js");
+const functionAjuste = require("./functionAjuste.js");
 const uploadFile = require("./app.js");
-const { response } = require('express');
-const xlxs = require("xlsx")
-
-
+const { response } = require("express");
+const xlxs = require("xlsx");
 
 const verescala = db.Escala;
- 
-async function rodarApp(arquivo){
 
-
-const result = await  excelToJson({
+async function rodarApp(arquivo) {
+  const result = await excelToJson({
     source: arquivo,
-    columnToKey:infoDados,
-    sheets:"base"
-});
+    columnToKey: infoDados,
+    sheets: "base",
+  });
 
-function agruparPor(objetoArray, propriedade){
-    const resultadoFinal = []
+  function agruparPor(objetoArray, propriedade) {
+    const resultadoFinal = [];
     return objetoArray.reduce(function (acc, obj) {
-        let key = obj[propriedade];
-        if (!acc[key]) {
-          acc[key] = [];
-          acc[key].push({
-            N_rota:obj["Id_Rota"],
-            Placa:"",
-            Transporte:String(obj["Transporte"]),
-            NF:functionAjuste.agruparNota(result.base,obj["Id_Rota"], obj["Remessa"] ),
-            Cliente:obj["Cliente"],
-            Cidade:obj["Cidade"],
-            Bairro:obj["Bairro"],
-            QtdEntrega:String(functionAjuste.contar(result.base,obj["Id_Rota"])),
-            Reentrega:String(functionAjuste.somarReentrega(result.base,obj["Id_Rota"],"Peso")),
-            LDB:String(functionAjuste.somar(result.base,obj["Id_Rota"],"4000", "Peso")),
-            ITB:String(functionAjuste.somar(result.base,obj["Id_Rota"],"3000", "Peso")),
-        })
-
+      let key = obj[propriedade];
+      if (!acc[key]) {
+        acc[key] = [];
+        acc[key].push({
+          N_rota: obj["Id_Rota"],
+          Placa: obj["Placa"],
+          Transporte: String(obj["Transporte"]),
+          NF: functionAjuste.agruparNota(
+            result.base,
+            obj["Id_Rota"],
+            obj["Remessa"]
+          ),
+          Cliente: obj["Cliente"],
+          Cidade: obj["Cidade"],
+          Bairro: obj["Bairro"],
+          QtdEntrega: String(
+            functionAjuste.contar(result.base, obj["Id_Rota"])
+          ),
+          Reentrega: String(
+            functionAjuste.somarReentrega(result.base, obj["Id_Rota"], "Peso")
+          ),
+          LDB: String(
+            functionAjuste.somar(result.base, obj["Id_Rota"], "4000", "Peso")
+          ),
+          ITB: String(
+            functionAjuste.somar(result.base, obj["Id_Rota"], "3000", "Peso")
+          ),
+        });
       }
-      return acc
-
+      return acc;
     }, {});
   }
 
-let escala = agruparPor(result.base, 'Id_Rota');
+  let escala = agruparPor(result.base, "Id_Rota");
 
-const novaEscala = Object.entries(escala)
+  const novaEscala = Object.entries(escala);
 
-const arraynovoFinal = novaEscala.reduce((acc, item)=> {
-    acc.push(item[1][0])
-    return acc
-},[])
+  const arraynovoFinal = novaEscala.reduce((acc, item) => {
+    acc.push(item[1][0]);
+    return acc;
+  }, []);
 
-arraynovoFinal.shift()
+  arraynovoFinal.shift();
 
-arraynovoFinal.map(async item=>{
-
+  arraynovoFinal.map(async (item) => {
     const validar = await verescala.count({
-        where:{
-            NRota: item.N_rota
-        }
-    })
+      where: {
+        NRota: item.N_rota,
+      },
+    });
 
-    if(validar < 1) {
-        const input = await verescala.create({
-            NRota: item.N_rota,
-            Transporte: item.Transporte,
-            Nf: item.NF,
-            Cliente: item.Cliente,
-            Cidade: item.Cidade,
-            Bairro: item.Bairro,
-            Qtdentregas: item.QtdEntrega,
-            Reentrega: item.Reentrega,
-            LDB: item.LDB,
-            ITB: item.ITB
-        })
+    if (validar < 1) {
+      const input = await verescala.create({
+        NRota: item.N_rota,
+        Placa: item.Placa,
+        Transporte: item.Transporte,
+        Nf: item.NF,
+        Cliente: item.Cliente,
+        Cidade: item.Cidade,
+        Bairro: item.Bairro,
+        Qtdentregas: item.QtdEntrega,
+        Reentrega: item.Reentrega,
+        LDB: item.LDB,
+        ITB: item.ITB,
+      });
+    }
+  });
 
-    }   
-
-})
-
-
-return arraynovoFinal
-
-
+  return arraynovoFinal;
 }
 
-module.exports = rodarApp
+module.exports = rodarApp;
--- src/routes/carregarArquivo/carregarArquivo.js
+++ src/routes/carregarArquivo/carregarArquivo.js
@@ -1,42 +1,43 @@
 const express = require("express");
-const multer = require('multer');
-const path = require('path');
-const rodarApp = require("../../converter/index.js")
-const fs = require("fs")
-const uploadFile = require("../../converter/app.js")
+const multer = require("multer");
+const path = require("path");
+const rodarApp = require("../../converter/index.js");
+const fs = require("fs");
+const uploadFile = require("../../converter/app.js");
 
-
-const storage = multer.memoryStorage()
+const storage = multer.memoryStorage();
 const storage2 = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, '/uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-  })
+  destination: function (req, file, cb) {
+    cb(null, "/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
 
-const upload = multer({ storage,storage2 });
+const upload = multer({ storage, storage2 });
 
 const path1 = path.resolve("uploads");
 
-
 const router = express.Router();
 
-router.post("/cadastro", upload.single('foto'), async (req, res) => {
-    //uploadFile(req.file)
-    rodarApp(req.file.buffer).then(data=> { 
-        res.json(data)})})
-    //const arquivo = fs.readFileSync(req.file.filename.b, "utf8")
-    //console.log(arquivo)
-    //res.json(req.file.fieldname)})
+router.post("/cadastro", upload.single("foto"), async (req, res) => {
+  //uploadFile(req.file)
+  rodarApp(req.file.buffer).then((data) => {
+    res.json(data);
+  });
+});
+//const arquivo = fs.readFileSync(req.file.filename.b, "utf8")
+//console.log(arquivo)
+//res.json(req.file.fieldname)})
 
 router.post("/rodar", async (req, res) => {
-    //rodarArquivo("5c8b8a03bc51aebc7759b60a23588c84904b2370c99a04289221fe77df2a8fd63f7bf5c1aecd19a486cfc5481209934eeb8c2ad3830ff9d74bde3af852c02585.xlsx")
-    res.json("OK")});
-router.get("/cadastro", (req,res)=> {
-    //wb.write('ArquivoExcel.xlsx', res)
+  //rodarArquivo("5c8b8a03bc51aebc7759b60a23588c84904b2370c99a04289221fe77df2a8fd63f7bf5c1aecd19a486cfc5481209934eeb8c2ad3830ff9d74bde3af852c02585.xlsx")
+  res.json("OK");
+});
+router.get("/cadastro", (req, res) => {
+  //wb.write('ArquivoExcel.xlsx', res)
 });
 
 module.exports = router;
