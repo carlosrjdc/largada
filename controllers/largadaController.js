c362ca2c977e5a8b62f24b889ffca8a5601bb329
ROSA Carlos <carlos.rosa@br.lactalis.com>
2023-03-13 09:46:27 -0300

    first commit

--- controllers/largadaController.js
+++ controllers/largadaController.js
@@ -4,14 +4,13 @@ const { Op } = require("sequelize");
 
 const verescala = db.Escala;
 
-
 class largadaController {
   //CADASTRO DE USUARIO
   static deletarregistros = async (req, res) => {
     try {
       const registro = await verescala.destroy({
         where: {},
-        truncate: true
+        truncate: true,
       });
       res.status(200).json("registro deletados");
     } catch (erro) {
@@ -21,16 +20,14 @@ class largadaController {
 
   static atualizarDados = async (req, res) => {
     try {
-      const registro = await verescala.update(
-        req.body,{
-        where: {id:req.params.id},
+      const registro = await verescala.update(req.body, {
+        where: { id: req.params.id },
       });
       res.status(200).json("registro deletados");
     } catch (erro) {
       return res.status(500).json(erro.message);
     }
   };
-
   static buscarRegistros = async (req, res) => {
     try {
       const registro = await verescala.findAll();
@@ -42,46 +39,51 @@ class largadaController {
   static estatistica = async (req, res) => {
     try {
       const aSeparar = await verescala.count({
-        where:{
-            inicioSeparacao:{
-            [Op.is]:null
-        }
-        }
+        where: {
+          inicioSeparacao: {
+            [Op.is]: null,
+          },
+        },
       });
       const emSeparacao = await verescala.count({
-        where:{
-            inicioSeparacao:{
-            [Op.ne]:null
-        }
-        }
+        where: {
+          inicioSeparacao: {
+            [Op.ne]: null,
+          },
+        },
       });
       const AConfererir = await verescala.count({
-        where:{
-            fimSeparacao:{
-            [Op.ne]:null
-        }
-        }
+        where: {
+          fimSeparacao: {
+            [Op.ne]: null,
+          },
+        },
       });
       const EmConferencia = await verescala.count({
-        where:{
-            inicioCarregamento:{
-            [Op.ne]:null
-        }
-        }
+        where: {
+          inicioCarregamento: {
+            [Op.ne]: null,
+          },
+        },
       });
       const Carregado = await verescala.count({
-        where:{
-            fimCarregamento:{
-            [Op.ne]:null
-        }
-        }
+        where: {
+          fimCarregamento: {
+            [Op.ne]: null,
+          },
+        },
       });
-      res.status(200).json({"ASeparar": aSeparar, "EmSeparação": emSeparacao, "AConferior":AConfererir, "EmConferencia":EmConferencia,"Carregado":Carregado});
+      res.status(200).json({
+        ASeparar: aSeparar,
+        EmSeparação: emSeparacao,
+        AConferior: AConfererir,
+        EmConferencia: EmConferencia,
+        Carregado: Carregado,
+      });
     } catch (erro) {
       return res.status(500).json(erro.message);
     }
   };
-
 }
 
 module.exports = largadaController;
--- models/escala.js
+++ models/escala.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 const Sequelize = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Escala extends Model {
@@ -14,76 +12,81 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Escala.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
+  Escala.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      NRota: {
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      Placa: {
+        type: Sequelize.STRING,
+      },
+      Transporte: {
+        type: Sequelize.STRING,
+      },
+      Nf: {
+        type: Sequelize.STRING,
+      },
+      Cliente: {
+        type: Sequelize.STRING,
+      },
+      Cidade: {
+        type: Sequelize.STRING,
+      },
+      Bairro: {
+        type: Sequelize.STRING,
+      },
+      Qtdentregas: {
+        type: Sequelize.INTEGER,
+      },
+      Reentrega: {
+        type: Sequelize.INTEGER,
+      },
+      LDB: {
+        type: Sequelize.INTEGER,
+      },
+      ITB: {
+        type: Sequelize.INTEGER,
+      },
+      inicioSeparacao: {
+        type: Sequelize.DATE,
+      },
+      fimSeparacao: {
+        type: Sequelize.DATE,
+      },
+      inicioCarregamento: {
+        type: Sequelize.DATE,
+      },
+      fimCarregamento: {
+        type: Sequelize.DATE,
+      },
+      cargaparada: {
+        type: Sequelize.STRING,
+      },
+      obs: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
     },
-    NRota: {
-      unique:true,
-      type: Sequelize.STRING
-    },
-    Placa: {
-      type: Sequelize.STRING
-    },
-    Transporte: {
-      type: Sequelize.STRING
-    },
-    Nf: {
-      type: Sequelize.STRING
-    },
-    Cliente: {
-      type: Sequelize.STRING
-    },
-    Cidade: {
-      type: Sequelize.STRING
-    },
-    Bairro: {
-      type: Sequelize.STRING
-    },
-    Qtdentregas: {
-      type: Sequelize.INTEGER
-    },
-    Reentrega: {
-      type: Sequelize.INTEGER
-    },
-    LDB: {
-      type: Sequelize.INTEGER
-    },
-    ITB: {
-      type: Sequelize.INTEGER
-    },
-    inicioSeparacao: {
-
-      type: Sequelize.DATE
-    },
-    fimSeparacao: {
-
-      type: Sequelize.DATE
-    },
-    inicioCarregamento: {
-
-      type: Sequelize.DATE
-    },
-    fimCarregamento: {
-
-      type: Sequelize.DATE
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: new Date()
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE,
-      defaultValue: new Date()
+    {
+      sequelize,
+      modelName: "Escala",
     }
-  }, {
-    sequelize,
-    modelName: 'Escala',
-  });
+  );
   return Escala;
 };
